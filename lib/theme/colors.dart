c5dc25126e364187a6213cc29a108791a5cf48e9
Emily Augusto <118476576+emilyaugusto@users.noreply.github.com>
2025-05-08 12:15:58 -0300

    Delete lib directory

--- lib/theme/colors.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-var primaryColor = const Color.fromARGB(255, 136, 46, 39);
-var secondaryColor = const Color.fromARGB(255, 148, 76, 71);
-var tertiaryColor = const Color(0xffffffff);
